3856f54b3a0d04c4e0608a74274f1133c277f3f8
Aarav Kalia <21202@wc.school.nz>
2024-06-20 11:09:16 +1200

    I have made my data desplayable on the website

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+DATABSE = "webtags.db"
+
+
+def create_connection(db_filename):
+    try:
+        connection = sqlite3.connect(db_filename)
+        return connection
+
+    except Error as e:
+        print(e)
+        return None
+
+
+
 
 
 @app.route('/')
-def home_page():  # put application's code here
-    return render_template('index.html')
+def home_page():
+
+    query = "SELECT id, Player FROM web_tags"
 
 
+
+    connection = create_connection(DATABSE)
+
+    cursor = connection.cursor()
+    cursor.execute(query)
+
+    data_list = cursor.fetchall()
+    print(data_list)
+
+    return render_template('index.html', data = data_list)
 
 if __name__ == '__main__':
     app.run()
